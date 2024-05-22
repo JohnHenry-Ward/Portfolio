d4a5683be66005c07c31839a945782ecfba73444
JohnHenry-Ward <johnhenry514@gmail.com>
2024-05-22 14:52:52 -0700

    more css for homepage, variable splash text and color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,25 @@
+import { Rampart_One } from "next/font/google";
+import Link from "next/link";
+const rampart_one = Rampart_One({ subsets: ['latin'], weight: '400' });
 
 const NavBar = () => {
   return (
-    <div id="navbar">
-      <div>
-        <h4>Home</h4>
-      </div>
-      <div>
-        <h4>About Me</h4>
-      </div>
-      <div>
-        <h4>Experience</h4>
-      </div>
-      <div>
-        <h4>Projects</h4>
-      </div>
-      <div>
-        <h4>Resume</h4>
-      </div>
+    <div id="navbar" className={rampart_one.className}>
+      <Link className="navbar-link" href='/'>
+        <h4 className="navbar-text">Home</h4>
+      </Link>
+      <Link className="navbar-link" href='/about'>
+        <h4 className="navbar-text">About Me</h4>
+      </Link>
+      <Link className="navbar-link" href='/experience'>
+        <h4 className="navbar-text">Experience</h4>
+      </Link>
+      <Link className="navbar-link" href='/'>
+        <h4 className="navbar-text">Projects</h4>
+      </Link>
+      <Link className="navbar-link" href='/'>
+        <h4 className="navbar-text">Resume</h4>
+      </Link>
     </div>
   )
 }
