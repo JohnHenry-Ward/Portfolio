ce1ce3cba3d4c8a2e7c861c5249f902a80b3e2af
JohnHenry-Ward <johnhenry514@gmail.com>
2024-05-28 18:20:51 -0700

    simplified active link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,49 +2,27 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 
 const NavBar = () => {
 
   const pathname = usePathname();
 
-  const [linkColors, setLinkColors] = useState({
-    'home': 'green',
-    'about': 'black',
-    'experience': 'black',
-    'projects': 'black',
-    'resume': 'black'
-  });
-
-  const onClick = (currentPage: String) => {
-    const newLinkColors = {
-      'home': currentPage == 'home' ? 'green' : 'black',
-      'about': currentPage == 'about' ? 'green' : 'black',
-      'experience': currentPage == 'experience' ? 'green' : 'black',
-      'projects': currentPage == 'projects' ? 'green' : 'black',
-      'resume': currentPage == 'resume' ? 'green' : 'black',
-    }
-
-    setLinkColors(newLinkColors);
-  }
-
-
   return (
     <div className='navbar'>
-      <Link className="navbar-link" href='/' onClick={(e) => onClick('home')}>
-        <h4 className={`navbar-text ${linkColors['home']}`}>Home</h4>
+      <Link className={`navbar-link`} href='/' >
+        <h4 className={`navbar-text ${pathname === '/' ? 'green' : 'black'}`}>Home</h4>
       </Link>
-      <Link className="navbar-link" href='/about' onClick={(e) => onClick('about')}>
-        <h4 className={`navbar-text ${linkColors['about']}`}>About Me</h4>
+      <Link className={`navbar-link`} href='/about' >
+        <h4 className={`navbar-text ${pathname === '/about' ? 'green' : 'black'}`}>About Me</h4>
       </Link>
-      <Link className="navbar-link" href='/experience' onClick={(e) => onClick('experience')}>
-        <h4 className={`navbar-text ${linkColors['experience']}`}>Experience</h4>
+      <Link className={`navbar-link`} href='/experience' >
+        <h4 className={`navbar-text ${pathname === '/experience' ? 'green' : 'black'}`}>Experience</h4>
       </Link>
-      <Link className="navbar-link" href='/projects' onClick={(e) => onClick('projects')}>
-        <h4 className={`navbar-text ${linkColors['projects']}`}>Projects</h4>
+      <Link className={`navbar-link`} href='/projects' >
+        <h4 className={`navbar-text ${pathname === '/projects' ? 'green' : 'black'}`}>Projects</h4>
       </Link>
-      <Link className="navbar-link" href='/resume' onClick={(e) => onClick('resume')}>
-        <h4 className={`navbar-text ${linkColors['resume']}`}>Resume</h4>
+      <Link className={`navbar-link`} href='/resume' >
+        <h4 className={`navbar-text ${pathname === '/resume' ? 'green' : 'black'}`}>Resume</h4>
       </Link>
     </div>
   )
