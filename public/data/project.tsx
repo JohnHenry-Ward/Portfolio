e6d533aa326dba01cb49e8b2e62472cf57d65790
JohnHenry-Ward <johnhenry514@gmail.com>
2024-06-20 15:13:18 -0700

    add projects to data file, with array of images/descriptions, and tech stack used

--- public/data/project.tsx
+++ public/data/project.tsx
@@ -2,8 +2,8 @@ interface Project {
     title: string,
     subtitle: string,
     body: string,
-    imgName: string,
-    imgDesc: string,
+    imgName: string[],
+    imgDesc: string[],
     key: number
   }
 
@@ -12,33 +12,49 @@ const data: Array<Project> = [
         "title": "Fantasy Futbol",
         "subtitle": "JavaScript, React.js, Node.js, MongoDB, API Integration, Heroku",
         "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sapiente qui minus minima doloribus, repellat voluptas non ullam, pariatur delectus distinctio nemo asperiores, magni voluptates vel sed itaque libero. Illo, vero optio suscipit dolore facere, quos atque aut commodi sit libero quaerat vel nobis! Facilis, quidem! Modi, placeat quo? Debitis aut voluptate exercitationem dignissimos nemo perferendis tempore, quae voluptas in ad. Natus ex accusantium vitae eum fugiat ipsum! Pariatur similique hic ad libero nihil eos voluptas dignissimos natus, ipsa, error eum illum totam sint distinctio iure soluta at. Voluptate unde a amet quaerat id ex reiciendis deserunt placeat molestias iusto.",
-        "imgName": "temp.png",
-        "imgDesc": "description of image",
+        "imgName": ["temp.png"],
+        "imgDesc": ["description of image"],
         "key": 1
     },
     {
         "title": "Movie Script Sentiment Analysis",
         "subtitle": "Python, NLP",
         "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sapiente qui minus minima doloribus, repellat voluptas non ullam, pariatur delectus distinctio nemo asperiores, magni voluptates vel sed itaque libero. Illo, vero optio suscipit dolore facere, quos atque aut commodi sit libero quaerat vel nobis! Facilis, quidem! Modi, placeat quo? Debitis aut voluptate exercitationem dignissimos nemo perferendis tempore, quae voluptas in ad. Natus ex accusantium vitae eum fugiat ipsum! Pariatur similique hic ad libero nihil eos voluptas dignissimos natus, ipsa, error eum illum totam sint distinctio iure soluta at. Voluptate unde a amet quaerat id ex reiciendis deserunt placeat molestias iusto.",
-        "imgName": "temp.png",
-        "imgDesc": "description of image",
+        "imgName": ["sentiment1.png"],
+        "imgDesc": ["description of image"],
         "key": 2
     },
     {
-        "title": "Project 3",
-        "subtitle": "x, y, z",
+        "title": "Instructor Efficiency Web Tool",
+        "subtitle": "JavaScript, Node.js, Express.js, HTML, CSS",
         "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sapiente qui minus minima doloribus, repellat voluptas non ullam, pariatur delectus distinctio nemo asperiores, magni voluptates vel sed itaque libero. Illo, vero optio suscipit dolore facere, quos atque aut commodi sit libero quaerat vel nobis! Facilis, quidem! Modi, placeat quo? Debitis aut voluptate exercitationem dignissimos nemo perferendis tempore, quae voluptas in ad. Natus ex accusantium vitae eum fugiat ipsum! Pariatur similique hic ad libero nihil eos voluptas dignissimos natus, ipsa, error eum illum totam sint distinctio iure soluta at. Voluptate unde a amet quaerat id ex reiciendis deserunt placeat molestias iusto.",
-        "imgName": "temp.png",
-        "imgDesc": "description of image",
+        "imgName": ["caaml1.png"],
+        "imgDesc": ["description of image"],
         "key": 3
     },
     {
-        "title": "Project 4",
-        "subtitle": "a, b, c",
+        "title": "Deadwood",
+        "subtitle": "Java, OOP, Pair Programming",
         "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sapiente qui minus minima doloribus, repellat voluptas non ullam, pariatur delectus distinctio nemo asperiores, magni voluptates vel sed itaque libero. Illo, vero optio suscipit dolore facere, quos atque aut commodi sit libero quaerat vel nobis! Facilis, quidem! Modi, placeat quo? Debitis aut voluptate exercitationem dignissimos nemo perferendis tempore, quae voluptas in ad. Natus ex accusantium vitae eum fugiat ipsum! Pariatur similique hic ad libero nihil eos voluptas dignissimos natus, ipsa, error eum illum totam sint distinctio iure soluta at. Voluptate unde a amet quaerat id ex reiciendis deserunt placeat molestias iusto.",
-        "imgName": "temp.png",
-        "imgDesc": "description of image",
+        "imgName": ["deadwood1.png"],
+        "imgDesc": ["description of image"],
         "key": 4
+    },
+    {
+        "title": "Microshell",
+        "subtitle": "C, UNIX",
+        "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sapiente qui minus minima doloribus, repellat voluptas non ullam, pariatur delectus distinctio nemo asperiores, magni voluptates vel sed itaque libero. Illo, vero optio suscipit dolore facere, quos atque aut commodi sit libero quaerat vel nobis! Facilis, quidem! Modi, placeat quo? Debitis aut voluptate exercitationem dignissimos nemo perferendis tempore, quae voluptas in ad. Natus ex accusantium vitae eum fugiat ipsum! Pariatur similique hic ad libero nihil eos voluptas dignissimos natus, ipsa, error eum illum totam sint distinctio iure soluta at. Voluptate unde a amet quaerat id ex reiciendis deserunt placeat molestias iusto.",
+        "imgName": ["microshell1.png"],
+        "imgDesc": ["description of image"],
+        "key": 5
+    },
+    {
+        "title": "Buy Better",
+        "subtitle": "JavaScript, Express.js, REST API, HTML, CSS",
+        "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sapiente qui minus minima doloribus, repellat voluptas non ullam, pariatur delectus distinctio nemo asperiores, magni voluptates vel sed itaque libero. Illo, vero optio suscipit dolore facere, quos atque aut commodi sit libero quaerat vel nobis! Facilis, quidem! Modi, placeat quo? Debitis aut voluptate exercitationem dignissimos nemo perferendis tempore, quae voluptas in ad. Natus ex accusantium vitae eum fugiat ipsum! Pariatur similique hic ad libero nihil eos voluptas dignissimos natus, ipsa, error eum illum totam sint distinctio iure soluta at. Voluptate unde a amet quaerat id ex reiciendis deserunt placeat molestias iusto.",
+        "imgName": ["buybetter.png"],
+        "imgDesc": ["description of image"],
+        "key": 6
     },
 ]
 
